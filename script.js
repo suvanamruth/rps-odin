84f8ffb49a8e88bfae34c3b4a141d4e0fa8868e8
suvanamruth <suvanamruth123@gmail.com>
2023-09-23 23:49:31 -0700

    Added counter for player and computer points. Check for who gets to 5 points first.

--- script.js
+++ script.js
@@ -50,11 +50,39 @@ function game(plchoice)
     playerChoice = plchoice
     computerChoice = getComputerChoice()
     result = playRound(playerChoice, computerChoice)
+    if(result.includes("Win"))
+    {
+        playerPoints = playerPoints + 1
+    }
+    else if(result.includes("Lose"))
+    {
+        computerPoints = computerPoints + 1
+    }
+    
     resultBox = document.querySelector('.result')
+    cPointsBox = document.querySelector('.computerCount')
+    pPointsBox = document.querySelector('.playerCount')
+    finalBox = document.querySelector('.final')
+
+    pPointsBox.textContent = "Player's points: " + playerPoints
+    cPointsBox.textContent = "Computer's points: " + computerPoints
     resultBox.textContent = result
+    if(computerPoints == 5)
+    {
+        finalBox.textContent = "Computer reached 5 points first. It wins!"
+    }
+    else if(playerPoints == 5)
+    {
+        finalBox.textContent = "You reached 5 points first. You win!"
+    }
+        
+
+    
 
     
 }
+playerPoints = 0
+computerPoints = 0
 function playScissors()
 {
     game("scissors")
